c370e2aa2abe9f32caf140c05a9041e168193a4f
Mubashira M K <mubashiramk07@gmail.com>
2022-10-11 16:00:12 +0530

    Add files via upload
    First commit

---
+++ src/components/About/AboutCard.js
@@ -0,0 +1,39 @@
+import React from "react";
+import Card from "react-bootstrap/Card";
+import { ImPointRight } from "react-icons/im";
+
+function AboutCard() {
+  return (
+    <Card className="quote-card-view">
+      <Card.Body>
+        <blockquote className="blockquote mb-0">
+          <p style={{ textAlign: "justify" }}>
+            Hi Everyone, I am <span className="purple">Mubashira M K </span>
+            from <span className="purple"> Kerala, India.</span>
+            <br />I am a Frontend developer at{" "}
+            <span className="purple"> XPAYBACK.</span>
+            <br />
+            <br />
+            Apart from coding, some other activities that I love to do!
+          </p>
+          <ul>
+            <li className="about-activity">
+              <ImPointRight /> Games & Sports
+            </li>
+            <li className="about-activity">
+              <ImPointRight /> Drawing
+            </li>
+            <li className="about-activity">
+              <ImPointRight /> Travelling
+            </li>
+          </ul>
+
+          <p style={{ color: "rgb(155 126 172)" }}>"Sharp the edge !" </p>
+          <footer className="blockquote-footer">Mubashira</footer>
+        </blockquote>
+      </Card.Body>
+    </Card>
+  );
+}
+
+export default AboutCard;
---
+++ src/components/Projects/Projects.js
@@ -0,0 +1,89 @@
+import React from "react";
+import { Container, Row, Col } from "react-bootstrap";
+import ProjectCard from "./ProjectCards";
+import Particle from "../Particle";
+import leaf from "../../Assets/Projects/leaf.png";
+import emotion from "../../Assets/Projects/emotion.png";
+import editor from "../../Assets/Projects/codeEditor.png";
+import covid from "../../Assets/covid.png";
+import suicide from "../../Assets/Projects/suicide.png";
+import bitsOfCode from "../../Assets/Projects/blog.png";
+import myTeamDashboard from "../../Assets/dashboard.png";
+import eventRegistration from "../../Assets/eventRegistration.png";
+import tracker from "../../Assets/tracker.png";
+import attendance from "../../Assets/attendance.png";
+
+function Projects() {
+  return (
+    <Container fluid className="project-section">
+      <Particle />
+      <Container>
+        <h1 className="project-heading">
+          My Recent <strong className="purple">Works </strong>
+        </h1>
+        <p style={{ color: "white" }}>
+          Here are a few projects I've worked on recently.
+        </p>
+        <Row style={{ justifyContent: "center", paddingBottom: "10px" }}>
+          <Col md={4} className="project-card">
+            <ProjectCard
+              imgPath={myTeamDashboard}
+              isBlog={false}
+              title="My Team Dashboard"
+              description=" Dashboard created using React to keep track of Team member’s activities, and schedule tasks. It also allow the team member authentication and admin authentication. Included the calender, activity analysis tab and employee report generator."
+              ghLink="https://github.com/Mubashiramk/MyTeamDashboard"
+              demoLink="https://myteamdashboard-bb0e6.web.app/"
+            />
+          </Col>
+
+          <Col md={4} className="project-card">
+            <ProjectCard
+              imgPath={eventRegistration}
+              isBlog={false}
+              title="Event Registration App"
+              description="Event registration app is a MERN stack project. Admin can add the events list, view the registered user, and edit the registered user data. User can login and add register. Backend is created using NodeJS, Express, and MongoDB."
+              ghLink="https://github.com/Mubashiramk/Event-Registration-User"
+              demoLink="https://github.com/Mubashiramk/Event-Registration-User-Backend"
+            />
+          </Col>
+
+          <Col md={4} className="project-card">
+            <ProjectCard
+              imgPath={covid}
+              isBlog={false}
+              title="Covid Tracker"
+              description="Covid dashboard created using React to keep track of country-wise Covid Active, Recovered, and Deceased
+              status. Created a backend server to ingest the data from API. Graphical analysis of country wise covid-19 cases."
+              ghLink="https://github.com/Mubashiramk/COVID19-Dashboard"
+              demoLink="https://covid19dashboard-339da.web.app/"
+            />
+          </Col>
+
+          <Col md={4} className="project-card">
+            <ProjectCard
+              imgPath={tracker}
+              isBlog={false}
+              title="Expense Tracker"
+              description="Expense tracker app to keep track of daily expenses and analyze the monthly and yearly expenses. Developed using React and Tailwind CSS."
+              ghLink="https://github.com/Mubashiramk/Expense-Tracker"
+              demoLink="https://myreact-646e7.web.app/"
+            />
+          </Col>
+
+          <Col md={4} className="project-card">
+            <ProjectCard
+              imgPath={attendance}
+              isBlog={false}
+              title="FingerPrint Attendance System"
+              description="Developed and Deployed Fingerprint Attendance System for college. Fingerprint-based authentication and authorization system using Python (Django). Uploading the attendance in college official site RSMS."
+              ghLink="https://github.com/Mubashiramk/AttendanceFingerprint"
+              // demoLink="https://www.youtube.com/watch?v=dQw4w9WgXcQ&ab_channel=RickAstley" <--------Please include a demo link here
+            />
+          </Col>
+        </Row>
+      </Container>
+    </Container>
+  );
+}
+
+export default Projects;
